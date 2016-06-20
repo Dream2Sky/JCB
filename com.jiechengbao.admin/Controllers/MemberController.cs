1bd3212b177ec43fe1405fe51464503d17e85402
Petrel <Petrel@DESKTOP-79970N2>
2016-06-20 09:06:17 +0800

    完成会员管理

---
+++ com.jiechengbao.admin/Controllers/MemberController.cs
@@ -0,0 +1,23 @@
+﻿using com.jiechengbao.Ibll;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace com.jiechengbao.admin.Controllers
+{
+    public class MemberController:Controller
+    {
+        private IMemberBLL _memberBLL;
+        public MemberController(IMemberBLL memberBLL)
+        {
+            _memberBLL = memberBLL;
+        }
+        public ActionResult List()
+        {
+            ViewData["MemberList"] = _memberBLL.GetAllNoDeletedMembers();
+            return View();
+        }
+    }
+}
