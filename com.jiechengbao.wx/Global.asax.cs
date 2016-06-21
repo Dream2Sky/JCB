5b0a234ff91454ff913aec2bbd6e7cd3836505b1
Petrel <Petrel@DESKTOP-79970N2>
2016-06-21 20:04:12 +0800

    添加MysqlEF  添加Osharp.AutoFac.MVC

--- com.jiechengbao.wx/Global.asax.cs
+++ com.jiechengbao.wx/Global.asax.cs
@@ -1,6 +1,13 @@
-﻿using System;
+﻿using Autofac;
+using Autofac.Integration.Mvc;
+using com.jiechengbao.bll;
+using com.jiechengbao.dal;
+using com.jiechengbao.Ibll;
+using com.jiechengbao.Idal;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Routing;
@@ -11,8 +18,33 @@ namespace com.jiechengbao.wx
     {
         protected void Application_Start()
         {
+            var builder = new ContainerBuilder();
+            SetupResolveRules(builder);
+            builder.RegisterControllers(Assembly.GetExecutingAssembly());
+
+            var container = builder.Build();
+            DependencyResolver.SetResolver(new AutofacDependencyResolver(container));
+
             AreaRegistration.RegisterAllAreas();
             RouteConfig.RegisterRoutes(RouteTable.Routes);
+        }
+
+        private void SetupResolveRules(ContainerBuilder builder)
+        {
+            builder.RegisterType<AdminDAL>().As<IAdminDAL>();
+            builder.RegisterType<AdminBLL>().As<IAdminBLL>();
+            builder.RegisterType<MemberDAL>().As<IMemberDAL>();
+            builder.RegisterType<MemberBLL>().As<IMemberBLL>();
+            builder.RegisterType<OrderDAL>().As<IOrderDAL>();
+            builder.RegisterType<OrderBLL>().As<IOrderBLL>();
+            builder.RegisterType<CategoryBLL>().As<ICategoryBLL>();
+            builder.RegisterType<CategoryDAL>().As<ICategoryDAL>();
+            builder.RegisterType<GoodsBLL>().As<IGoodsBLL>();
+            builder.RegisterType<GoodsDAL>().As<IGoodsDAL>();
+            builder.RegisterType<GoodsCategoryDAL>().As<IGoodsCategoryDAL>();
+            builder.RegisterType<GoodsCategoryBLL>().As<IGoodsCategoryBLL>();
+            builder.RegisterType<GoodsImagesBLL>().As<IGoodsImagesBLL>();
+            builder.RegisterType<GoodsImagesDAL>().As<IGoodsImagesDAL>();
         }
     }
 }
