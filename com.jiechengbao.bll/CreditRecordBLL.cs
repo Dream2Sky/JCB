2aaff011b939bd247f0ed69fcd96a650950935f7
Petrel <Petrel@DESKTOP-79970N2>
2016-07-03 17:27:26 +0800

    添加CreditRecord实体类的相关数据访问层和业务层接口和类

---
+++ com.jiechengbao.bll/CreditRecordBLL.cs
@@ -0,0 +1,19 @@
+﻿using com.jiechengbao.Ibll;
+using com.jiechengbao.Idal;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace com.jiechengbao.bll
+{
+    public class CreditRecordBLL:ICreditRecordBLL
+    {
+        private ICreditRecordDAL _creditRecordDAL;
+        public CreditRecordBLL(ICreditRecordDAL creditRecordDAL)
+        {
+            _creditRecordDAL = creditRecordDAL;
+        }
+    }
+}
